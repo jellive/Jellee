f121f23373bb4200c7bef1a7cb345a155fc17fa9
Jellive <mystral7@naver.com>
2020-05-12 00:21:23 +0900

    init commit.

---
+++ src/background/background.ts
@@ -0,0 +1,22 @@
+// const isThisBackground = true;
+// console.log('isThisBackground', isThisBackground);
+
+const filter = {
+  urls: [
+    '*://facebook.com/*',
+  ],
+};
+
+const opt = ['blocking']
+
+window.chrome.webRequest.onBeforeRequest.addListener(
+  page => {
+    console.log('page blocked - ' + page.url);
+
+    return {
+      cancel: true,
+    };
+  },
+  filter,
+  opt
+);

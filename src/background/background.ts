a093edec4f244b474cf5bbbfab34659d47b9cad4
PD Jell <jellive7@gmail.com>
2020-05-26 19:15:14 +0900

    del arr to deep copy.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,20 +1,21 @@
 // const isThisBackground = true;
 // console.log('isThisBackground', isThisBackground);
+import { FILTER_ADDR } from '../popup/src/models'
 
 const filter = {
-    urls: ['*://facebook.com/*'],
+  urls: ['*://facebook.com/*'],
 }
 
 const opt = ['blocking']
 
 window.chrome.webRequest.onBeforeRequest.addListener(
-    (page) => {
-        console.log('page blocked - ' + page.url)
+  (page) => {
+    console.log('page blocked - ' + page.url)
 
-        return {
-            cancel: true,
-        }
-    },
-    filter,
-    opt,
+    return {
+      cancel: true,
+    }
+  },
+  filter,
+  opt,
 )
--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -7,12 +7,7 @@ import { addFilterAddr, delFilterAddr } from './apis'
 function App() {
 
   const [addr, setAddr] = React.useState('')
-
-
-
-
-  const [filteredAddr, setFilteredAddr] = React.useState<Array<string | null>>(JSON.parse(localStorage.getItem(FILTER_ADDR) || ''))
-
+  const [filteredAddr, setFilteredAddr] = React.useState<Array<string>>(JSON.parse(localStorage.getItem(FILTER_ADDR) || ''))
   const [delArr, setDelArr] = React.useState<Array<string>>([])
 
   const changeAddr = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,37 +15,30 @@ function App() {
   }
 
   const addAddr = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    addFilterAddr(addr)
+    addFilterAddr(`*://${addr}/*`)
     setAddr('')
     setFilteredAddr(JSON.parse(localStorage.getItem(FILTER_ADDR) || ''))
   }
 
   const checkDelArr = (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-    const tempArr = delArr
-    console.log('tempArr', typeof tempArr)
+    const tempArr = delArr.slice() // deep copy
     if (tempArr.indexOf(event.currentTarget.value) !== -1) tempArr.splice(tempArr.indexOf(event.currentTarget.value), 1)
     else tempArr.push(event.currentTarget.value)
-    console.log('tempArr', tempArr)
     setDelArr(tempArr)
-    console.log('delArr', delArr)
+  }
+
+  const delAddr = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const tempArr = delArr.slice()
+    tempArr.forEach(addr => delFilterAddr(addr))
+    setAddr('')
+    setDelArr([])
+    setFilteredAddr(JSON.parse(localStorage.getItem(FILTER_ADDR) || ''))
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>hi?</p>
-        <p>{localStorage.getItem(FILTER_ADDR)}</p>
         <p>delArr: {delArr}</p>
         {filteredAddr && filteredAddr.map((element: string | null, index) =>
           <React.Fragment key={index}><input type="checkbox" onClick={checkDelArr} value={element || ''} /><p>{element}</p></React.Fragment>
@@ -58,6 +46,7 @@ function App() {
         <input onChange={changeAddr} value={addr} placeholder="입력해볼래?" />
         <p>{addr}</p>
         <button onClick={addAddr}>삽입</button>
+        <button onClick={delAddr}>삭제</button>
       </header>
     </div>
   );
